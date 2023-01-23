1f8ce0cd987632da2cf012d10d967690bc6d11a5
annawattman <annalinneahs@hotmail.com>
2023-01-23 11:41:16 +0100

    Initial Commit

---
+++ virtualHamster/virtualHamsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualHamsterApp.swift
+//  virtualHamster
+//
+//  Created by Anna Wattman on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct virtualHamsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
